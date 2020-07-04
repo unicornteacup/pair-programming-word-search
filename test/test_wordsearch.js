20fc615c53be540eaffd32179049e8e09a00b518
Caroline <carolinejepps@gmail.com>
2020-07-04 01:58:07 +0000

    Completed pair programming

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word is not present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +35,51 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return false if the word is not present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY')
+
+    assert.isTrue(result);
+  });
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], 'LARRY')
+
+    assert.isFalse(result);
+  });
 });
+
+
+// check to find horizontally and vertically 
+// return true if found and false if not found
+// fix existing code
+// add test to check if empty array
+// add test for if word is vertical
+
+//OPTIONAL
+//test for backwards and diagonal
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,88 @@
 const wordSearch = (letters, word) => { 
+    if (letters.length === 0) {
+        return false
+    }
+    const newArray = [];
+    for (let x = 0; x < letters.length; x++) {
+        newArray[x] = [];
+        for (let y = 0; y < letters.length; y++) {
+        newArray[x][y] = letters[y][x];
+        }
+      }
+    const verticalJoin = newArray.map(ls => ls.join(''));
     const horizontalJoin = letters.map(ls => ls.join(''))
+   
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        for (k of verticalJoin) {
+        if (l.includes(word) || k.includes(word)) return true;
+        }
+    }
+    for (l of horizontalJoin) {
+        for (k of verticalJoin) {
+        if (!l.includes(word) || !k.includes(word)) return false;
+        }
     }
 }
-
 module.exports = wordSearch
+
+
+    // for (l of verticalJoin) {
+    //     if (l.includes(word)) return true;
+    // }
+    // for (l of verticalJoin) {
+    //     if (!l.includes(word)) return false;
+    // }
+
+
+    // const verticals = [];
+    // for (let row = 0; row < rowLength; row++) {
+    //     for (let column = 0; column < letters.length; column++) {
+    //      verticals.push(column);
+    //     }
+    // }
+
+        
+        // if (hasHor === true || hasVer === true) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+    
+
+    // console.log(verticals);
+    // const verticalJoin = verticals.join('');
+    // for (l of verticalJoin) {
+    //     if (l.includes(word)) return true
+    // }
+    // for (l of verticalJoin) {
+    //     if (!l.includes(word)) return false
+    // }
+
+//     const verticalJoin = verticals.map(ls => ls.join(''));
+//     for (l of verticalJoin) {
+//         if (l.includes(word)) hasVer = true;
+//     }
+//     for (l of verticalJoin) {
+//         if (!l.includes(word)) hasVer = false;
+//     }
+//     if (hasHor === true || hasVer === true) {
+//         return true;
+//     } else {
+        // return false;
+//     }
+// }
+
+
+
+
+
+
+
+// check to find horizontally and vertically 
+// return true if found and false if not found
+// fix existing code
+// add test to check if empty array
+// add test for if word is vertical
+
+//OPTIONAL
+//test for backwards and diagonal
